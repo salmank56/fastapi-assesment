b2af9a693439b5ffa9d684a285da41ad5ff2ff25
Salman Khan <sk5926255@gmail.com>
2024-12-06 03:03:16 +0500

    Added

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Replace with your actual Snowflake connection details
-DATABASE_URL = "snowflake://user:password@account/db/schema"
+DATABASE_URL = "snowflake://fastapi_user:test123@kvigpva/fastapi_db/public"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
